5c20b0effa61a2a9d0c1ac638509a5ff5c2b7243
Scott Klakken <scott@northernpacific.tech>
2025-02-07 11:02:21 -0800

    Tenant Tester

---
+++ tenant/t_tenant.sql
@@ -0,0 +1,61 @@
+-- ----------------------------------------------------------------------------
+-- © 2025 Northern Pacific Technologies, LLC.
+-- Licensed under the MIT License.
+-- See LICENSE file in the project root for full license information.
+-- ----------------------------------------------------------------------------
+
+drop procedure if exists pareto.t_tenant;
+create procedure pareto.t_tenant()
+language plpgsql
+as $$
+declare
+ 
+  c_test_name   constant varchar := 't_tenant';
+  c_name        constant varchar := 't_tenant';
+  c_description constant varchar := 't_description';
+  c_copyright   constant varchar := 't_copyright';
+  c_username    constant varchar := 't_username';
+  v_id          uuid;        
+  v_response    pareto.response;
+  
+begin
+
+  -- ----------------------------------
+  -- Clean if necessary
+  -- ----------------------------------
+  select id into v_id 
+    from pareto.tenant 
+   where name = c_name;
+   
+  if (v_id is not null) then
+    raise notice 'WARNING: tenant value "%" existed from a previous test. Deleting...', c_name;
+    call pareto.d_tenant_by_alt_key(c_name, c_username, v_response);
+    raise notice '%, %, %, %', v_response.success, v_response.id, v_response.updated, v_response.message;
+  end if;
+
+  -- ----------------------------------
+  -- Insert
+  -- ----------------------------------
+  call pareto.i_tenant(c_name, c_description, c_copyright, 'Scott1', v_response);
+  raise notice '%, %, %, %', v_response.success, v_response.id, v_response.updated, v_response.message;
+  assert v_response.success = true, 'Test failed: i_tenant was not successful. See logs for details.';
+  
+  -- ----------------------------------
+  -- Update
+  -- ----------------------------------
+  call pareto.u_tenant(v_response.id, c_name, c_description, c_copyright, 'Scott2', v_response);
+  raise notice '%, %, %, %', v_response.success, v_response.id, v_response.updated, v_response.message;
+  assert v_response.success = true, 'Test failed: u_tenant was not successful. See logs for details.';
+
+  -- ----------------------------------
+  -- Delete
+  -- ----------------------------------
+  call pareto.d_tenant_by_alt_key(c_name, c_username, v_response);
+  raise notice '%, %, %, %', v_response.success, v_response.id, v_response.updated, v_response.message;
+  assert v_response.success = true, 'Test failed: d_tenant was not successful. See logs for details.';
+  
+end;
+$$;
+
+call pareto.t_tenant();
+drop procedure if exists pareto.t_tenant;
--- utils/tester.bat
+++ utils/tester.bat
@@ -5,17 +5,6 @@ rem Licensed under the MIT License.
 rem See LICENSE file in the project root for full license information.
 rem ---------------------------------------------------------------------------
 
-rem Recreate the norpac schema
-psql -d norpac -h localhost -p 5432 -f ".\rebuild.sql"
+rem Run Tests
 
-rem Create Tables 
-
-psql -d norpac -h localhost -p 5432 -f "..\logs\c_logs.sql"
-psql -d norpac -h localhost -p 5432 -f "..\tenant\c_tenant.sql"
-psql -d norpac -h localhost -p 5432 -f "..\ref_table_type\c_ref_table_type.sql"
-psql -d norpac -h localhost -p 5432 -f "..\ref_tables\c_ref_tables.sql"
-
-rem Create Stored Procedures 
-
-psql -d norpac -h localhost -p 5432 -f "..\logs\s_logs.sql"
-psql -d norpac -h localhost -p 5432 -f "..\tenant\s_tenant.sql"
+psql -d norpac -h localhost -p 5432 -f "..\tenant\t_tenant.sql"
