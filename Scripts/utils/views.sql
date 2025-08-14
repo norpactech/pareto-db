11ced06cea2e53741953c94fb4d9faa775214d93
Scott Klakken <scott@northernpacific.tech>
2025-08-13 17:08:48 -0700

    Cleanup and Create Table Refactor in the Factory

--- Scripts/utils/views.sql
+++ Scripts/utils/views.sql
@@ -16,60 +16,21 @@ CREATE VIEW pareto.v_index AS
   JOIN pareto.property p             ON (p.id = ip.id_property)
   JOIN pareto.ref_tables rt          ON (rt.id = ip.id_rt_sort_order);
 
--- List all tables and order by least to most dependent 	
-
-DROP VIEW IF EXISTS pareto.v_table_dependencies2;
-CREATE VIEW pareto.v_table_dependencies2 AS
-  -- has dependencies
-  SELECT t.id     AS id_tenant,
-         t.name   AS tenant_name,
-		 s.id     AS id_schema,
-         s.name   AS schema_name, 
-		 o1.id    AS id_data_object,
-         o1.name  AS data_object_name,
-		 o2.id    AS id_object_dependency,
-		 o2.name  AS object_dependency_name
-    FROM pareto.data_object o1
-    JOIN pareto.schema s ON (s.id = o1.id_schema) 
-    JOIN pareto.tenant t ON (t.id = s.id_tenant)
-    JOIN pareto.property p ON (p.id_data_object = o1.id)
-    JOIN pareto.generic_property_type pt ON (pt.id = p.id_generic_property_type)
-    JOIN pareto.cardinality c ON (c.id_property = p.id)
-    JOIN pareto.data_object o2 ON (o2.id = c.id_data_object)
-   UNION
-  -- No dependencies
-  SELECT t.id     AS id_tenant,
-         t.name   AS tenant_name,
-		 s1.id    AS id_schema,
-         s1.name  AS schema_name, 
-		 o1.id    AS id_data_object,
-         o1.name  AS data_object_name,
-		 NULL     AS id_object_dependency,
-		 NULL     AS object_dependency_name
-    FROM pareto.data_object o1
-    JOIN pareto.schema s1 ON (s1.id = o1.id_schema) 
-    JOIN pareto.tenant t ON (t.id = s1.id_tenant)
-   WHERE o1.id not in (SELECT o.id 
-                         FROM pareto.data_object o
-                         JOIN pareto.schema s2 ON (s2.id = o.id_schema) 
-                         JOIN pareto.property p ON (p.id_data_object = o.id)
-                         JOIN pareto.generic_property_type pt ON (pt.id = p.id_generic_property_type)
-                         JOIN pareto.cardinality c ON (c.id_property = p.id)
-                        WHERE s2.id = s1.id);
-
-
-DROP VIEW IF EXISTS pareto.v_table_dependencies;
-CREATE VIEW pareto.v_table_dependencies AS
-  SELECT
-    tc.table_schema,
-    tc.table_name AS child_table,
-    ccu.table_name AS parent_table
-  FROM
-    information_schema.table_constraints AS tc
-    JOIN information_schema.key_column_usage AS kcu
-      ON tc.constraint_name = kcu.constraint_name
-      AND tc.table_schema = kcu.table_schema
-    JOIN information_schema.constraint_column_usage AS ccu
-      ON ccu.constraint_name = tc.constraint_name
-      AND ccu.table_schema = tc.table_schema
-   WHERE tc.constraint_type = 'FOREIGN KEY';
+DROP VIEW IF EXISTS pareto.v_object_dependencies CASCADE;
+CREATE VIEW pareto.v_object_dependencies AS
+SELECT 
+  t.id AS id_tenant,
+  t.name AS tenant_name,
+  s.id AS id_schema, 
+  s.name AS schema_name,
+  do1.id AS id_child_object,
+  do1.name AS child_object_name,
+  p.name AS property_name,
+  do2.id AS id_parent_object,
+  do2.name AS parent_object_name
+FROM pareto.property p
+JOIN pareto.data_object do1 ON (do1.id = p.id_data_object)
+JOIN pareto.cardinality c ON (c.id_property = p.id)
+JOIN pareto.data_object do2 ON (do2.id = c.id_data_object)
+JOIN pareto.schema s ON (s.id = do1.id_schema)
+JOIN pareto.tenant t ON (t.id = s.id_tenant);
